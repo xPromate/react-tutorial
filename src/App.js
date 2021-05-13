7defb78653423eb04ae82c7d47f47bad0a5d31ce
xPromate <jpcm_1998@hotmail.com>
2021-05-13 22:32:33 +0100

    react-tutorial 1st part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState(
+    [
+    {
+        id:1,
+        text:"fixe",
+        day:"Dia de são nunca à tarde",
+        reminder: false,
+    },
+    {
+        id:2,
+        text:"porreiro",
+        day:"Dia de são nunca à tarde",
+        reminder: true,
+    },
+    {
+        id:3,
+        text:"espectacular",
+        day:"Dia de são nunca à tarde de noite",
+        reminder: false,
+    }
+])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
+
 
 export default App;
